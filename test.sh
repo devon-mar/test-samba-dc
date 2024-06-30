f439509697e10138e7544127b7a207f12dd63589
Devon Mar <devonm@mdmm.ca>
2024-06-29 20:12:29 -0700

    Test container

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+for i in {0..2}
+do
+  ldapsearch -v -x -H ldap://localhost -D 'EXAMPLE\administrator' -w 'ohfie3Uahe' -b 'DC=example,DC=com' '(objectClass=user)'
+  if [ "$?" -eq 0 ]; then
+    exit 0
+  fi
+  echo "ldapsearch failed $i"
+  sleep 2
+done
+
+echo "ldapsearch failed"
+exit 1
