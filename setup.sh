e96947db1be578db421114f2df4c2d903ddf971a
devon-mar <devon-mar@users.noreply.github.com>
2024-10-10 19:31:28 -0700

    Disable password expiry

--- setup.sh
+++ setup.sh
@@ -14,9 +14,13 @@ samba-tool domain provision \
 sed -i '/\[global\]/a ldap server require strong auth = no' /etc/samba/smb.conf
 sed -i '/\[global\]/a log level = 3' /etc/samba/smb.conf
 
+samba-tool user setexpiry Administrator --noexpiry
 samba-tool user create user1 Password1
+samba-tool user setexpiry user1 --noexpiry
 samba-tool user create user2 Password2
+samba-tool user setexpiry user2 --noexpiry
 samba-tool user create user3 Password3
+samba-tool user setexpiry user3 --noexpiry
 
 samba-tool group add group1
 samba-tool group addmembers group1 user1
